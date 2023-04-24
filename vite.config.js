6899adca806b569bf752c8a401eb486a97c2a470
keshiecodes <morahthankgod@gmail.com>
2023-04-24 21:58:50 +0100

    lo

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    https: false,
+  resolve: {
+    alias: {
+      "@": path.join(__dirname, "/resources/ts/src/"),
+    },
   },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
+  base: "/"
 })
