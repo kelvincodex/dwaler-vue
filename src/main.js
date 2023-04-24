42a7bc817e3baceb49ac1102628d105c1a51078e
keshiecodes <morahthankgod@gmail.com>
2023-04-24 17:50:25 +0100

    klickf

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router/index.js";
 import {createMetaManager} from "vue-meta";
+import LoadScript from "vue-plugin-load-script";
 
 
 
 const app = createApp(App)
     app.use(router)
     app.use(createMetaManager())
+    app.use(LoadScript)
     app.mount('#app')
